0db2a8afe889648912b0fe2063bb8a3e5da7c77c
Kuanysh <KBokeikhanov@beeline.kz>
2021-12-19 13:20:29 +0600

    Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+
+        int a = scanner.nextInt();
+        System.out.println("You entered the number:" + a);
+        System.out.println("Enter the second number:");
+
+        int b = scanner.nextInt();
+        int c = a + b;
+        System.out.println("Result:" + c);
+    }
+}
